420119ea9b1b8f3553707557dc45769ade514338
Ariel Orozco <bassplayer85@gmail.com>
2023-11-09 17:25:43 -0600

    Second update 2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("#send").click(function(){
+    $("#send").click(function () {
         console.log('Sending ...');
 
         var datos = {
             id: "Donald Duck",
             title: "Duckburg"
-          };
+        };
 
         $.ajax({
             url: "https://c64-diagnostic.fly.dev/diagnose",
@@ -14,20 +14,11 @@ $(document).ready(function(){
             dataType: "json",
             data: JSON.stringify(datos),
             contentType: 'application/json; charset=utf-8',
-            success: function(result, status) {
+            success: function (result, status) {
                 console.log("Data: " + result + "\nStatus: " + status);
                 // });
             }
-  });      
-  
-        // $.post("https://c64-diagnostic.fly.dev/diagnose",
-        // {
-        //   id: "Donald Duck",
-        //   title: "Duckburg"
-        // },
-        // function(data, status){
-        //   alert("Data: " + data + "\nStatus: " + status);
-        // });
-      });
-  
-  });
+        });
+    });
+
+});
