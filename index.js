869aa974a9f17ff60ad74d81776b9a6911839069
Ariel Orozco <bassplayer85@gmail.com>
2023-11-09 17:03:29 -0600

    First deploy

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    $("#send").click(function(){
+        console.log('Sending ...');
+
+        var datos = {
+            id: "Donald Duck",
+            title: "Duckburg"
+          };
+
+        $.ajax({
+            url: "https://c64-diagnostic.fly.dev/diagnose",
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify(datos),
+            contentType: 'application/json; charset=utf-8',
+            success: function(result, status) {
+                alert("Data: " + data + "\nStatus: " + status);
+                // });
+            }
+  });      
+  
+        // $.post("https://c64-diagnostic.fly.dev/diagnose",
+        // {
+        //   id: "Donald Duck",
+        //   title: "Duckburg"
+        // },
+        // function(data, status){
+        //   alert("Data: " + data + "\nStatus: " + status);
+        // });
+      });
+  
+  });
