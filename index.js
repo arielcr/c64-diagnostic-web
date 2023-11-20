45cb3836bdbffc5297aa15cb11f7531046093607
Ariel Orozco <bassplayer85@gmail.com>
2023-11-20 14:08:48 -0600

    Finished templating

--- index.js
+++ index.js
@@ -1,59 +1,81 @@
 $(document).ready(function () {
 
     //const DIAG_API = "https://c64-diagnostic.fly.dev"
-    const DIAG_API = "http://localhost:8080"
+    const DIAG_API = "http://localhost:8080";
+
+    var metadata = {
+        precheck: "",
+        system_power_supply: "",
+        system_reset: "",
+        system_clocks: "",
+        system_video: "",
+        system_ram: "",
+        system_rom_io: ""
+    };
+
+    $.ajax({
+        url: DIAG_API + "/metadata",
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            metadata.precheck = data.precheck;
+            metadata.system_power_supply = data.system_power_supply;
+            metadata.system_reset = data.system_reset;
+            metadata.system_clocks = data.system_clocks;
+            metadata.system_video = data.system_video;
+            metadata.system_ram = data.system_ram;
+            metadata.system_rom_io = data.system_rom_io;
+        }
+    });
 
     $("#start").click(function () {
-        console.log('Sending ...');
-
         var diagnostic = {
             diagnostic: "precheck",
             step: 1,
             result: ""
         };
+        diagnose(diagnostic);
+    });
 
+    $(document).on('click', '#continue', function () {
+        var $answer = $("input[name='answer']:checked");
+
+        if ($answer.length > 0) {
+            $(".error").hide();
+
+            var diagnostic = {
+                diagnostic: "precheck",
+                step: 1,
+                result: $answer.val()
+            };
+
+            diagnose(diagnostic);
+        } else {
+            $(".error").show();
+        }
+    });
+
+    function diagnose(diagnostic) {
         $.ajax({
             url: DIAG_API + "/diagnose",
             type: "POST",
             dataType: "json",
             data: JSON.stringify(diagnostic),
             contentType: 'application/json; charset=utf-8',
-            success: function (result) {
-
-                console.log("test", result.test);
-                
-                $.get("templates/prechecks/b1.html", function (html_string) {
-                    // Log the received HTML string
-                    console.log("Received HTML:", html_string);
-                
-                    // Create a jQuery object for the HTML string
-                    var modifiedHtml = $(html_string);
-                
-                    // Modify the elements directly
-                    modifiedHtml.find('.question').html(result.test);
-                    modifiedHtml.find('.test-info').html(result.type + " | Step " + result.step);
-                
-                    // Log the modified HTML string
-                    console.log("Modified HTML:", modifiedHtml);
-                
-                    // Update the content of .diagnostic with the modified HTML
-                    $(".diagnostic").html(modifiedHtml);
+            success: function (data) {
+                $.get("templates/test.html", function (template) {
+                    var html = template
+                        .replace("{{step}}", data.step)
+                        .replace("{{type}}", (data.type).replace(/_/g, ' '))
+                        .replace("{{category}}", data.category)
+                        .replace("{{test}}", data.test)
+                        .replace("{{description}}", metadata[data.type]);
+                    $(".diagnostic").html(html);
                 }, 'html');
-                
-          
             }
         });
-    });
+    }
 
-    $(document).on('change', 'input[type=radio][name=answer]', function() {
-        console.log(this.value);
-        if (this.value == 'true') {
-            $(".fail").hide();
-            $(".pass").show();
-        } else if (this.value == 'false') {
-            $(".pass").hide();
-            $(".fail").show();
-        }
-    });
 
 });
